68e73cc50534710db3e2813011de7c38dfbd1fad
Nandu <nanduwastaken@gmail.com>
2022-11-20 11:39:25 +0530

    don

--- delete.js
+++ delete.js
@@ -8,6 +8,3 @@ rest.put(Routes.applicationCommands(clientId), { body: [] })
     .then(() => console.log('Successfully deleted all application commands.'))
     .catch(console.error);
 
-rest.put(Routes.applicationGuildCommands(clientId, guildId), { body: [] })
-	.then(() => console.log('Successfully deleted all guild commands.'))
-	.catch(console.error);
