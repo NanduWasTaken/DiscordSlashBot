2936daa653190351688fb3363b9527dcd3824ccf
Nandu <nanduwastaken@gmail.com>
2022-11-20 17:05:19 +0530

    mongoose database for econom

---
+++ events/database.js
@@ -0,0 +1,23 @@
+module.exports = {
+    name:'database',
+    execute(client) {
+     const mongoose = require('mongoose');
+     const db = mongoose.connection
+
+
+
+     mongoose.connect(process.env.DPASS, {
+       useNewUrlParser: true,
+         useUnifiedTopology: true
+         });
+         db.once('open', () => {
+           console.log('Successfully connected to MongoDB using Mongoose!'.green)
+         });
+         db.on('error', () => {
+           console.log('MongoDB Connection Error'.red)
+         });
+
+
+    }
+    
+};
---
+++ models/DiscordUser.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+const DiscordUserSchema = new mongoose.Schema({
+        discordId: { type: String, required: true },
+        username: { type: String, required: true },
+        money: { type: String, require: true },
+        avatar: { type: String, required: true }
+     });
+
+ module.exports = mongoose.model('discord_users', DiscordUserSchema);
+
