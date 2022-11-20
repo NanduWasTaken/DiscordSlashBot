70614fde61174a76de4328ab6b6bc62a5eeb5063
Nandu <nanduwastaken@gmail.com>
2022-11-20 17:58:06 +0530

    e

--- database.js
+++ database.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose');
 
 
-mongoose.connect('mongodb://localhost:27017/usersdb',
-  {
+mongoose.connect(process.env.DPASS,
+ {
       useNewUrlParser: true,
       useFindAndModify: false,
       useUnifiedTopology: true
