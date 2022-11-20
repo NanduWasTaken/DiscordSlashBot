f101e433e8867b259d0e454772116cce290b077d
Nandu <nanduwastaken@gmail.com>
2022-11-20 11:27:48 +0530

    hjo

--- delete.js
+++ delete.js
@@ -1,14 +1,7 @@
 const { REST, Routes } = require('discord.js');
 const { clientId, guildId } = require('./config.json');
 const token = process.env["TOKEN"];
-const rest = new REST({ version: '10' }).setToken(token);
-
-// ...
-
-// for guild-based commands
-rest.put(Routes.applicationGuildCommands(clientId, guildId), { body: [] })
-	.then(() => console.log('Successfully deleted all guild commands.'))
-    .catch(console.error);
+const rest = new REST({ version: '10' }).setToken(token)
 
         // for global commands
 rest.put(Routes.applicationCommands(clientId), { body: [] })
