f7f85045de6590390029e9a9196c59299005bcee
Nandu <nanduwastaken@gmail.com>
2022-11-20 12:06:02 +0530

    Server Numbe

--- events/ready.js
+++ events/ready.js
@@ -4,6 +4,7 @@ module.exports = {
      once: true,
     	execute(client) {
          console.log(`Ready! Logged in as ${client.user.tag}`);
+         console.log(`${client.user.tag} is on ${client.guilds.cache.size} servers!`)
          client.user.setPresence({
           activities: [{ name: `Mighty Walnut`, type: ActivityType.Playing }],
      	status: 'dnd',
