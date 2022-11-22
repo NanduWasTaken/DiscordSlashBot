604ca4ac2a21e7ceb6f88d980cdc2ef26d8e009c
Nandu <nanduwastaken@gmail.com>
2022-11-22 19:15:34 +0530

    m

--- commands/create.js
+++ commands/create.js
@@ -7,15 +7,7 @@ module.exports = {
         .setDescription('Replies with Pong!'),
         async execute(interaction) {
             const user = interaction.user;
-             const newUsers = new Users({
-                 id: user.id,
-                 tag: user.tag,
-                 bal: "1000"
-         });   
 
-         newUser.save().catch(err => {
-                 console.error(err)
-           })
            return interaction.reply('created!')
     },
  };
