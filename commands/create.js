0e586f22714a28e7ac45e4e026817c00e10421c7
Nandu <nanduwastaken@gmail.com>
2022-11-22 19:00:39 +0530

    fixe

--- commands/create.js
+++ commands/create.js
@@ -1,12 +1,12 @@
 const { SlashCommandBuilder } = require('discord.js');
 const Users = require('../models/DiscordUser.js')
-const user = interaction.user;
 
 module.exports = {
 	data: new SlashCommandBuilder()
     	.setName('create')
         .setDescription('Replies with Pong!'),
         async execute(interaction) {
+            const user = interaction.user;
              const newUsers = new Users({
                  id: user.id,
                  tag: user.tag,
